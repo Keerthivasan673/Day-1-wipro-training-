84aaaf4110c4bd180d60aba91570fd30ef6cf817
Keerthivasan673 <89631327+Keerthivasan673@users.noreply.github.com>
2023-02-07 07:02:01 +0530

    Create Loop3.java

---
+++ Loop3.java
@@ -0,0 +1,13 @@
+package loop1;
+
+public class loop3 {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+for(int i=0;i<=100;i++) {
+System.out.println(i);
+i++;
+}
+}
+
+}
