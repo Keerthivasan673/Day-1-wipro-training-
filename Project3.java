301b2b3a1a3e701fc70bfc27fc162b73e09f33f1
Keerthivasan673 <89631327+Keerthivasan673@users.noreply.github.com>
2023-02-07 06:42:54 +0530

    Create Project3.java

---
+++ Project3.java
@@ -0,0 +1,20 @@
+ // write a program to accept a number N and print whether the number is EVEN or ODD
+package day1;
+import java.util.*;
+
+public class project3 {
+
+public static void main(String[] args) {
+System.out.print("Enter number :");
+Scanner sc= new Scanner(System.in);
+int inp=sc.nextInt();
+if(inp%2==0) {
+System.out.println("EVEN");
+}
+else {
+System.out.println("ODD");
+}
+
+}
+
+}
